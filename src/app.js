8387992d1cbdc24c80e21ffda9f90fa5436076c9
Mateus Louzada <mateuslouzada01@gmail.com>
2022-09-18 13:38:48 -0300

    Adicionando cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use((_, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "*");
   app.use(cors());
   next();
 });
